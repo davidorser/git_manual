9eb06c3f43f0a4283f84f195859a8e4e15c87852
David Orser <davidorser@yahoo.com>
2015-11-23 12:26:33 -0600

    Initial Check In iwth modification

---
+++ djoHelloWorld/src/djoHelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : djoHelloWorld.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!! djo" << endl; // prints !!!Hello World!!!
+	return 0;
+}
